2a86ec5839b63bc4e43962a0865605cb796a7f2f
Slavbor <shipmate@mail.ru>
2022-12-16 15:42:34 +0300

    Zadacha_2 & Zadacha_4 ready

---
+++ Zadach4/Program.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console;
+
+Write("Введите число а: ");
+int a = int.Parse(ReadLine()!);
+Write("Введите число b: ");
+int b = int.Parse(ReadLine()!);
+Write("Введите число c: ");
+int c = int.Parse(ReadLine()!);
+if (a > b && a > c)
+{
+    WriteLine("max =" + a);
+}
+else if (b > a && b > c)
+{
+    WriteLine("max = " + b);
+}
+else if (c > a && c > b)
+{
+    WriteLine("max = " + c);
+}
+else if (c == a && a == b)
+{
+    WriteLine("Числа равны.");
+}
+
---
+++ Zadacha2/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+
+Write("Введите число а: ");
+int a = int.Parse(ReadLine()!);
+Write("Введите число b: ");
+int b = int.Parse(ReadLine()!);
+if (a > b)
+{
+    WriteLine("max =" + a);
+}
+else if (a == b)
+{
+    WriteLine("Числа равны.");
+}
+else
+{
+    WriteLine("max = " + b);
+}
