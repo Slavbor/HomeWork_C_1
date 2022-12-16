6189a09587cd82339e9695bd7519bfbf1246ed6a
Slavbor <shipmate@mail.ru>
2022-12-16 16:00:42 +0300

    git add execute

---
+++ Zadacha6/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+
+Write("Введите целое число: ");
+int a = int.Parse(ReadLine()!);
+
+if (a % 2 == 0) // остаток от деления
+{
+    WriteLine("Да");
+}
+else
+{
+    WriteLine("Нет");
+}
