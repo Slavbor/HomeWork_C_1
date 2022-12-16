072aa27e0c3440098768fc84c92a50739d0d16d8
Slavbor <shipmate@mail.ru>
2022-12-16 16:11:11 +0300

    Zadacha8(chet) ready

---
+++ Zadacha8/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+
+Write("Введите целое число: ");
+int n = int.Parse(ReadLine()!);
+int count = 1;
+
+while (count != n + 1)
+{
+    if (count % 2 == 0)
+        WriteLine(count);
+    count++;
+}
